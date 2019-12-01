bb436421bfe619a039ba7420e5fdd8a2a58ed3be
Matt Kern <mjkern@andrew.cmu.edu>
2019-12-01 14:48:36 -0500

    gets the hot posts from a subreddit

--- downloader.py
+++ downloader.py
@@ -1,7 +1,20 @@
 
+# initialization
 import praw
 reddit = praw.Reddit()
 
-for submission in reddit.front.hot(limit=10):
-    print(submission.score)
+# constants
+limit = 13
+subreddit_name = None # set to None to get name at runtime
+
+# user input
+if subreddit_name:
+    print("using subreddit: " + subreddit_name)
+else:
+    subreddit_name = input("subreddit: ")
+
+# print all the submission titles
+print("------ top " + str(limit) + " posts ------")
+for submission in reddit.subreddit(subreddit_name).hot(limit=limit):
+    print(submission.title)
 
