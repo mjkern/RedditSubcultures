7ee64b2353db2338698e9fd35f991a104abf4594
Matt Kern <mjkern@andrew.cmu.edu>
2019-12-01 15:36:50 -0500

    trying to save content

--- downloader.py
+++ downloader.py
@@ -1,7 +1,26 @@
 
-# initialization
+# setup
 import praw
 reddit = praw.Reddit()
+
+import pandas as pd
+submissions = pd.DataFrame(columns=[
+    "id",
+    "user",
+    "date",
+    "score",
+    "title",
+    "body",
+    "subreddit"
+])
+
+comments = pd.DataFrame(columns=[
+    "id",
+    "submission_id",
+    "user",
+    "score",
+    "body"
+])
 
 # constants
 limit = 13
@@ -14,9 +33,20 @@ else:
     subreddit_name = input("subreddit: ")
 
 # print all the submission titles
-print("------ top " + str(limit) + " posts ------")
+print("------ top " + str(limit) + " submissions ------")
 for submission in reddit.subreddit(subreddit_name).new(limit=limit):
     print(submission.title)
+    submissions.append({
+        "id": submission.id,
+        "title": submission.title
+    }, ignore_index=True)
     for comment in submission.comments:
         print("\t" + comment.body)
+        comments.append({
+            "id": comment.id,
+            "body": comment.body
+        }, ignore_index=True)
 
+# write the output files
+submissions.to_csv("downloads/" + subreddit_name + "-submissions")
+comments.to_csv("downloads/" + subreddit_name + "-comments")
