7e0e417b3359e1450892c66b616f576cba953db8
Matt Kern <mjkern@andrew.cmu.edu>
2019-12-04 10:40:42 -0500

    now prints updates while downloaded

--- downloader.py
+++ downloader.py
@@ -27,12 +27,27 @@ comments = pd.DataFrame(columns=[
 # constants
 limit = 100000 # I suspect reddit will cap me out at some point
 subreddit_name = None # set to None to get name at runtime
+count_to_update = 10
 
 # get subreddit name
 if subreddit_name:
     print("using subreddit: " + subreddit_name)
 else:
     subreddit_name = input("subreddit: ")
+    
+# a helper to print periodic updates
+count_downloaded = 0
+last_update_string = ""
+print("downloaded ", end="")
+def update_count():
+    global count_downloaded # don't really know why python requires this...
+    global last_update_string # and this
+    count_downloaded += 1
+    if count_downloaded % count_to_update == 0:
+        print("\b" * len(last_update_string), end="") # backspace old update
+        last_update_string = str(count_downloaded)
+        print(last_update_string, end="")
+        
 
 # get submissions and comments
 for submission in reddit.subreddit(subreddit_name).new(limit=limit):
@@ -54,8 +69,10 @@ for submission in reddit.subreddit(subreddit_name).new(limit=limit):
             "score": comment.score,
             "body": comment.body
         }, ignore_index=True)
+    update_count()
 
 # give feedback
+print()
 print("got " + str(len(submissions)) + " submissions and " + str(len(comments)) + " comments")
 
 # write the output files
