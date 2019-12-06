964e7dee1603c1f74e74539ff84c4c298dd67295
Matt Kern <mjkern@andrew.cmu.edu>
2019-12-05 20:00:40 -0500

    downloader catches problems

--- downloader.py
+++ downloader.py
@@ -38,27 +38,33 @@ else:
 # get submissions and comments
 number_downloaded = 0
 for submission in reddit.subreddit(subreddit_name).new(limit=limit):
-    submissions = submissions.append({
-        "submission_id": submission.id,
-        "user": submission.author,
-        "utc": submission.created_utc,
-        "score": submission.score,
-        "title": submission.title,
-        "body": submission.selftext,
-        "subreddit": submission.subreddit.display_name
-    }, ignore_index=True)
-    for comment in submission.comments:
-        comments = comments.append({
-            "comment_id": comment.id,
-            "submission_id": comment.link_id,
-            "user": comment.author,
-            "utc": comment.created_utc,
-            "score": comment.score,
-            "body": comment.body
+    try:
+        submissions = submissions.append({
+            "submission_id": submission.id,
+            "user": submission.author,
+            "utc": submission.created_utc,
+            "score": submission.score,
+            "title": submission.title,
+            "body": submission.selftext,
+            "subreddit": submission.subreddit.display_name
         }, ignore_index=True)
-    # update count
-    number_downloaded += 1
-    print("downloaded " + str(number_downloaded), end="\r") # \r causes the next print to overwrite this one
+        for comment in submission.comments:
+            try:
+                comments = comments.append({
+                    "comment_id": comment.id,
+                    "submission_id": comment.link_id,
+                    "user": comment.author,
+                    "utc": comment.created_utc,
+                    "score": comment.score,
+                    "body": comment.body
+                }, ignore_index=True)
+            except:
+                print("one failed comment")
+        # update count
+        number_downloaded += 1
+        print("downloaded " + str(number_downloaded), end="\r") # \r causes the next print to overwrite this one
+    except:
+        print("one failed submission")
 
 # give feedback
 print()
